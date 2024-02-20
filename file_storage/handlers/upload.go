ced275c4449bf37e46bd07d4e83e14c4fb5c4cf0
Mario Daoud <mario.daoud.3002@gmail.com>
2024-02-20 21:40:19 +0100

    fixed multi file handling

--- file_storage/handlers/upload.go
+++ file_storage/handlers/upload.go
@@ -9,52 +9,64 @@ import (
 )
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
-	// parse data
-	err := r.ParseMultipartForm(0)
+	err := r.ParseMultipartForm(0) // 0 = unlimited size - change to put apply file size limit
 	if err != nil {
 		http.Error(w, "Unable to parse form file", http.StatusBadRequest)
 		return
 	}
 
-	// get file from form
-	file, handler, err := r.FormFile("file")
-	if err != nil {
-		http.Error(w, "Unable to get form from file", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
+	files := r.MultipartForm.File["files"]
 
-	filename := handler.Filename
-	path := filepath.Join("storage", filename)
-
-	// create file
-	out, err := os.Create(path)
-	if err != nil {
-		http.Error(w, "Unable to create file on server", http.StatusInternalServerError)
-		return
-	}
-	defer out.Close()
-
-	// copy file in chunks
-	chunksize := 8192
-	buffer := make([]byte, chunksize) // 8kb buffer for chunks
-	for {
-		n, err := file.Read(buffer)
-		if err == io.EOF {
-			break // stop when reached end of file
-		}
+	storageDir := "storage"
+	for _, fileHeader := range files {
+		file, err := fileHeader.Open()
 		if err != nil {
-			http.Error(w, "Error trying to read file", http.StatusInternalServerError)
+			http.Error(w, "Error opening uploaded file", http.StatusInternalServerError)
 			return
 		}
+		defer file.Close()
 
-		// write chunk to output
-		_, err = out.Write(buffer[:n])
+		filename := filepath.Join(storageDir, fileHeader.Filename)
+
+		if _, err := os.Stat(storageDir); os.IsNotExist(err) {
+			if err := os.MkdirAll(storageDir, 0777); err != nil {
+				http.Error(w, "Error creating directory", http.StatusInternalServerError)
+				return
+			}
+		}
+
+		out, err := os.Create(filename)
 		if err != nil {
-			http.Error(w, "Error trying to write to file", http.StatusInternalServerError)
+			http.Error(w, "Unable to create file on server", http.StatusInternalServerError)
 			return
 		}
-	}
+		defer out.Close()
 
-	fmt.Fprintf(w, "File %s uploaded successfully\n", filename)
+		// buffer for handling bigger files too
+		const chunkSize = 8192 // 8KB chunk size
+		buffer := make([]byte, chunkSize)
+
+		// copy file content to server in chunks
+		for {
+			// read chunk from the input file
+			n, err := file.Read(buffer)
+			if err == io.EOF {
+				break // end of file reached
+			}
+			if err != nil {
+				http.Error(w, "Error reading file", http.StatusInternalServerError)
+				return
+			}
+
+			// write chunk to output file
+			chunk := buffer[:n]
+			_, err = out.Write(chunk)
+			if err != nil {
+				http.Error(w, "Error writing file", http.StatusInternalServerError)
+				return
+			}
+		}
+
+		fmt.Fprintf(w, "File %s uploaded successfully\n", fileHeader.Filename)
+	}
 }
